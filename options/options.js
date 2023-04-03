e13b02ee5988d0783c741012b3f57f18e9fa1670
Crowdbotics <team@crowdbotics.com>
2023-04-03 07:29:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-butterfly-39681",
+  url: "https://divine-butterfly-39681.botics.co",
+  api: "https://divine-butterfly-39681.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
